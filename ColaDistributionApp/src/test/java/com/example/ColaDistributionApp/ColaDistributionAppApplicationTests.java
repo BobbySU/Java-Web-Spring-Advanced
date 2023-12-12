e69cbcc5a4ea654295ee9720f5ac6a410cd6c4b9
BobbySU <radio_20bg@yahoo.com>
2023-12-12 09:29:57 +0200

    Upload Empty Project

---
+++ ColaDistributionApp/src/test/java/com/example/ColaDistributionApp/ColaDistributionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ColaDistributionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColaDistributionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
